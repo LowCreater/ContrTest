d9eed6775256513cd183448e7b8fe0a984b7150b
Ivan <CHechikovIP@gmail.com>
2024-02-19 21:18:22 +0500

    работа с кодом завершена

--- TEST.py
+++ TEST.py
@@ -2,6 +2,6 @@ list1 = ["test", "Hello", "World", "123", "example", "CV", "hp;"]
 result_array = []
 for i in list1:
     if len(i) <= 3:
-        result_array = result_array.append(1)
+        result_array.append(i)
         
 print(result_array)
