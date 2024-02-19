eefeca166dfd97c20462976b3ac36ab4eda1feb9
Ivan <CHechikovIP@gmail.com>
2024-02-19 20:31:45 +0500

    create cycle

---
+++ TEST.py
@@ -0,0 +1,7 @@
+list1 = ["test", "Hello", "World", "123", "example", "CV", "hp;"]
+result_array = []
+for i in list1:
+    if len(i) <= 3:
+        result_array = result_array.append(1)
+        
+print(result_array)
